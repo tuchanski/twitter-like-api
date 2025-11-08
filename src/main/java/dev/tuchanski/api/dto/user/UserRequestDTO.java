6053da8fd929263726c9d08241ba3617cb664cdf
Tuchanski <gui.rocha2004@gmail.com>
2025-11-08 19:30:44 -0300

    feat: add error messages to dtos

--- src/main/java/dev/tuchanski/api/dto/user/UserRequestDTO.java
+++ src/main/java/dev/tuchanski/api/dto/user/UserRequestDTO.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 public record UserRequestDTO(
-        @NotEmpty @Length(min = 4, max = 12) String username,
-        @NotEmpty @Email String email,
-        @NotEmpty @Length(min = 4, max = 20) String password
+        @NotEmpty @Length(min = 4, max = 12, message = "Username must be between 4 and 12 characters") String username,
+        @NotEmpty(message = "E-mail is mandatory") @Email(message = "E-mail is not valid") String email,
+        @NotEmpty(message = "Password is mandatory") @Length(min = 4, max = 20, message = "Password must be between 4 and 20 characters") String password
 ) {
 }
--- src/main/java/dev/tuchanski/api/dto/user/UserUpdateDTO.java
+++ src/main/java/dev/tuchanski/api/dto/user/UserUpdateDTO.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 
 public record UserUpdateDTO(
-        @Length(min = 4, max = 12) String username,
-        @Email String email,
-        @Length(min = 4, max = 20) String password
+        @Length(min = 4, max = 12, message = "Username must be between 4 and 12 characters") String username,
+        @Email(message = "E-mail is not valid") String email,
+        @Length(min = 4, max = 20, message = "Password must be between 4 and 20 characters") String password
 ) {
 }
