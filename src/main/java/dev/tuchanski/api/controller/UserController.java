695efa7c88b37ef3ab02e1c81920eb6420cefbbd
Tuchanski <gui.rocha2004@gmail.com>
2025-11-08 20:17:38 -0300

    feat: add user controller

--- src/main/java/dev/tuchanski/api/controller/UserController.java
+++ src/main/java/dev/tuchanski/api/controller/UserController.java
@@ -2,15 +2,16 @@ package dev.tuchanski.api.controller;
 
 import dev.tuchanski.api.dto.user.UserRequestDTO;
 import dev.tuchanski.api.dto.user.UserResponseDTO;
+import dev.tuchanski.api.dto.user.UserUpdateDTO;
 import dev.tuchanski.api.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,6 +23,32 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserResponseDTO> create(@Valid @RequestBody UserRequestDTO userRequestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(userRequestDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<UserResponseDTO> findByUsername(@PathVariable String username) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findByUsername(username));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> update(@PathVariable UUID id, @RequestBody UserUpdateDTO userUpdateDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.update(id, userUpdateDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        userService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
