dcf27cf634f150de988f7b59dbb3dd42f92f6343
Tuchanski <gui.rocha2004@gmail.com>
2025-11-08 19:37:06 -0300

    feat: add interface for user service

---
+++ src/main/java/dev/tuchanski/api/service/user/UserService.java
@@ -0,0 +1,17 @@
+package dev.tuchanski.api.service.user;
+
+import dev.tuchanski.api.dto.user.UserRequestDTO;
+import dev.tuchanski.api.dto.user.UserResponseDTO;
+import dev.tuchanski.api.dto.user.UserUpdateDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserService {
+    UserResponseDTO create(UserRequestDTO userRequestDTO);
+    List<UserResponseDTO> findAll();
+    UserResponseDTO findById(UUID id);
+    UserResponseDTO findByUsername(String username);
+    UserResponseDTO update(UUID id, UserUpdateDTO userUpdateDTO);
+    void delete(UUID id);
+}
