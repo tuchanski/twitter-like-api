b0810a32e36d6bfe2dae9b9409c33c2aa4571f54
Tuchanski <gui.rocha2004@gmail.com>
2025-11-09 17:10:15 -0300

    feat: add impl to user details to user entity

---
+++ src/main/java/dev/tuchanski/api/entity/enums/UserRole.java
@@ -0,0 +1,17 @@
+package dev.tuchanski.api.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+}
