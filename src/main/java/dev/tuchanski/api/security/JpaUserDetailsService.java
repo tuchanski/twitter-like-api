f84af1db62a6a2278951a8d2b4038d5a5bfa9536
Tuchanski <gui.rocha2004@gmail.com>
2025-11-08 21:47:09 -0300

    feat: add jpa user details service

---
+++ src/main/java/dev/tuchanski/api/security/JpaUserDetailsService.java
@@ -0,0 +1,36 @@
+package dev.tuchanski.api.security;
+
+import dev.tuchanski.api.entity.User;
+import dev.tuchanski.api.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@AllArgsConstructor
+public class JpaUserDetailsService  implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // Using e-mail as username in security context
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with e-mail: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(Collections.emptyList())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
