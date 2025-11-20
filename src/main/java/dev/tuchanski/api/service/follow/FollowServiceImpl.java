5af4e47d7a97ab51064cb39111af8ae6c32dd945
Tuchanski <gui.rocha2004@gmail.com>
2025-11-20 13:18:04 -0300

    feat: add follow service & impl

---
+++ src/main/java/dev/tuchanski/api/service/follow/FollowServiceImpl.java
@@ -0,0 +1,106 @@
+package dev.tuchanski.api.service.follow;
+
+import dev.tuchanski.api.dto.follow.FollowResponseDTO;
+import dev.tuchanski.api.entity.Follow;
+import dev.tuchanski.api.entity.User;
+import dev.tuchanski.api.exception.follow.RelationshipAlreadyExistsException;
+import dev.tuchanski.api.exception.follow.RelationshipIsNotValidException;
+import dev.tuchanski.api.exception.follow.RelationshipNotFoundException;
+import dev.tuchanski.api.exception.user.UserNotFoundException;
+import dev.tuchanski.api.mapper.FollowMapper;
+import dev.tuchanski.api.repository.FollowRepository;
+import dev.tuchanski.api.repository.UserRepository;
+import dev.tuchanski.api.service.auth.TokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static dev.tuchanski.api.service.user.UserServiceImpl.getUser;
+
+@Service
+@AllArgsConstructor
+public class FollowServiceImpl implements FollowService {
+
+    private final FollowRepository followRepository;
+    private final UserRepository userRepository;
+    private final TokenService tokenService;
+    private final FollowMapper followMapper;
+
+    @Override
+    public FollowResponseDTO createFollow(String token, String usernameFollowTarget) {
+        User sessionUser = getUserFromToken(token);
+
+        User followTarget = (User) userRepository.findByUsername(usernameFollowTarget);
+
+        if (followTarget == null) {
+            throw new UserNotFoundException("User not found with username " + usernameFollowTarget);
+        }
+
+        if (sessionUser.getId().equals(followTarget.getId())) {
+            throw new RelationshipIsNotValidException("You are not allowed to follow yourself");
+        }
+
+        if (followRepository.existsByFollowerAndFollowed(sessionUser, followTarget)) {
+            throw new RelationshipAlreadyExistsException("This user is already following " + followTarget.getUsername());
+        }
+
+        Follow relationship = new Follow();
+
+        relationship.setFollower(sessionUser);
+        relationship.setFollowed(followTarget);
+
+        relationship = followRepository.save(relationship);
+
+        return followMapper.toDTO(relationship);
+    }
+
+    @Override
+    public List<FollowResponseDTO> getFollowing(String username) {
+        User user = (User) userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UserNotFoundException("User not found with name " + username);
+        }
+
+        return user.getFollowing().stream().map(followMapper::toDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<FollowResponseDTO> getFollowers(String username) {
+        User user = (User) userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UserNotFoundException("User not found with name " + username);
+        }
+
+        return user.getFollowers().stream().map(followMapper::toDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public void delete(String token, String usernameUnfollowTarget) {
+        User sessionUser = getUserFromToken(token);
+        User unfollowTarget = (User) userRepository.findByUsername(usernameUnfollowTarget);
+
+        if (unfollowTarget == null) {
+            throw new UserNotFoundException("User not found with name " + usernameUnfollowTarget);
+        }
+
+        if (sessionUser.getId().equals(unfollowTarget.getId())) {
+            throw new RelationshipIsNotValidException("You are not allowed to follow/unfollow yourself");
+        }
+
+        Follow follow = followRepository.findByFollowerAndFollowed(sessionUser, unfollowTarget);
+
+        if (follow == null) {
+            throw new RelationshipNotFoundException("Follow not found");
+        }
+
+        followRepository.delete(follow);
+    }
+
+    private User getUserFromToken(String token) {
+        return getUser(token, tokenService, userRepository);
+    }
+}
