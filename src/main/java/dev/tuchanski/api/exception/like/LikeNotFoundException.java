7395d83d4d70f0b04fd134896f6f0913c8f3fe16
Tuchanski <gui.rocha2004@gmail.com>
2025-11-20 11:14:07 -0300

    feat: add like not found exception

---
+++ src/main/java/dev/tuchanski/api/exception/like/LikeNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.tuchanski.api.exception.like;
+
+public class LikeNotFoundException extends RuntimeException {
+    public LikeNotFoundException(String message) {
+        super(message);
+    }
+}
