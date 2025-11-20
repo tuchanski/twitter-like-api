19c2d2afe2c20ec82093947896c83e9752f889bc
Tuchanski <gui.rocha2004@gmail.com>
2025-11-20 13:17:17 -0300

    feat: add custom queries in follow repo

--- src/main/java/dev/tuchanski/api/repository/FollowRepository.java
+++ src/main/java/dev/tuchanski/api/repository/FollowRepository.java
@@ -1,6 +1,7 @@
 package dev.tuchanski.api.repository;
 
 import dev.tuchanski.api.entity.Follow;
+import dev.tuchanski.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, UUID> {
-    boolean existsByFollowerId(UUID followerId, UUID followingId);
+    boolean existsByFollowerAndFollowed(User follower, User followed);
+    Follow findByFollowerAndFollowed(User follower, User followed);
 }
